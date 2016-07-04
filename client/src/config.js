244b79a9a685e895efee677ccc645fdaf3bfda19
ytyszxf <ljz135790@gmail.com>
2016-07-04 11:04:40 +0800

    save

--- client/src/config.js
+++ client/src/config.js
@@ -2,8 +2,8 @@
     window.appConfig = {
       "DEV": {
         "cloudUrl": "http://api-development-beehivecn3.internal.kii.com",
-        "wsUrl": "ws://114.215.196.178:8080/beehive-portal/websocket/stomp",
-        "siteUrl": "https://114.215.196.178:443",
+        "wsUrl": "http://114.215.196.178:8080/beehive-portal/websocket/stomp",
+        "siteUrl": "http://114.215.196.178:8080",
         "thirdPartyAPIUrl": "http://114.215.196.178:8081/3rdpartyapiserver/app/api_inquiry/apilist.html?vendor=Beehive"
       },
       "QA": {
