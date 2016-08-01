6cf763af57e64f8cfc18cc81f74bb7bb55f62206
augustine <ashramwen@gmail.com>
2016-08-01 18:08:24 +0800

    monitoring card

--- client/src/app/components/portal/MonitorManager/MonitorManager.js
+++ client/src/app/components/portal/MonitorManager/MonitorManager.js
@@ -1,1 +1,12 @@
-angular.module('BeehivePortal.MonitorManager', ['ui.router']);
+angular.module('BeehivePortal.MonitorManager', ['ui.router'])
+
+.constant('ThingType', {
+    'AirCondition': {
+        'Power': '电源',
+        'Speed': '风速',
+        'Mode': '运行模式',
+        'Temp': '温度',
+        'OPHours': '运行时常(小时)',
+        'ConStates': '连接状态'
+    }
+})
--- client/src/app/components/portal/MonitorManager/Monitoring/Monitoring.controller.js
+++ client/src/app/components/portal/MonitorManager/Monitoring/Monitoring.controller.js
@@ -14,6 +14,71 @@ angular.module('BeehivePortal.MonitorManager')
     $$User.getCustomData({ name: 'mv_' + $scope.view.id }).$promise.then(function(res) {
         $scope.view = res.view || {};
         websocketInit();
+        /*
+        {
+  "detail" : [ {
+    "modifyBy" : "493e83c9",
+    "vendorThingID" : "0807W-F02-03-118",
+    "kiiThingID" : "th.f83120e36100-bada-6e11-81f4-0a4538a6",
+    "select" : false,
+    "modifyDate" : 1469116800000,
+    "type" : "Lighting",
+    "tags" : [ ],
+    "kiiAppID" : "493e83c9",
+    "fullKiiThingID" : "493e83c9-th.f83120e36100-bada-6e11-81f4-0a4538a6",
+    "createBy" : "f1dadad0-4834-11e6-9c73-00163e007aba",
+    "globalThingID" : 1090,
+    "createDate" : 1469030400000,
+    "status" : {
+      "taiwanNo1" : 1469168587000,
+      "Power" : 0,
+      "target" : "th.f83120e36100-bada-6e11-81f4-0a4538a6"
+    }
+  }, {
+    "modifyBy" : "493e83c9",
+    "vendorThingID" : "0807W-F02-15-118",
+    "kiiThingID" : "th.aba700e36100-72a9-6e11-81f4-0b7146b6",
+    "select" : false,
+    "modifyDate" : 1469116800000,
+    "type" : "Lighting",
+    "tags" : [ ],
+    "kiiAppID" : "493e83c9",
+    "fullKiiThingID" : "493e83c9-th.aba700e36100-72a9-6e11-81f4-0b7146b6",
+    "createBy" : "f1dadad0-4834-11e6-9c73-00163e007aba",
+    "globalThingID" : 1091,
+    "createDate" : 1469030400000,
+    "status" : {
+      "taiwanNo1" : 1469168580000,
+      "Bri" : 284,
+      "target" : "th.aba700e36100-72a9-6e11-81f4-0b7146b6"
+    }
+  }, {
+    "modifyBy" : "493e83c9",
+    "vendorThingID" : "0807W-F02-03-117",
+    "kiiThingID" : "th.aba700e36100-72a9-6e11-81f4-071aaac6",
+    "select" : false,
+    "modifyDate" : 1469116800000,
+    "type" : "Lighting",
+    "tags" : [ ],
+    "kiiAppID" : "493e83c9",
+    "fullKiiThingID" : "493e83c9-th.aba700e36100-72a9-6e11-81f4-071aaac6",
+    "createBy" : "f1dadad0-4834-11e6-9c73-00163e007aba",
+    "globalThingID" : 1093,
+    "createDate" : 1469030400000,
+    "status" : {
+      "SetBri" : 0,
+      "Turntimes" : 0,
+      "taiwanNo1" : 1469168581000,
+      "State" : 0,
+      "Alarm" : 0,
+      "Bri" : 0,
+      "Runtimes" : 0,
+      "Power" : 0,
+      "target" : "th.aba700e36100-72a9-6e11-81f4-071aaac6"
+    }
+  } ]
+}
+*/
     })
 
     // websocket connection
