2dc09a31a8c45a8c1361c26a6800979ed21895cd
augustine <ashramwen@gmail.com>
2016-08-03 13:53:51 +0800

    monitoring

--- client/src/app/components/portal/MonitorManager/MonitorManager.const.js
+++ client/src/app/components/portal/MonitorManager/MonitorManager.const.js
@@ -10,10 +10,18 @@ angular.module('BeehivePortal.MonitorManager')
 })
 
 .constant('StateType', {
-    'AirCondition': '空调',
-    'FreshAir': '新风',
-    'EnvironmentSensor': '环境传感器',
-    'Lighting': '照明',
-    'ElectricMeter': '智能电表',
-    'AirCleaner': '空气净化器'
+    'AFStates': '滤网状态',
+    'AirQuality': '空气质量',
+    'Bri': '亮度',
+    'ColTemp': '色温',
+    'Col': '颜色',
+    'ConStates': '连接状态',
+    'Inverter': '变频频率',
+    'FeedBack': '变频反馈',
+    'Mode': '运行模式',
+    'OPStates': '运行状态',
+    'OPHours': '运行时长',
+    'Power': '电源',
+    'Speed': '风速',
+    'Temp': '温度'
 });
--- client/src/app/components/portal/MonitorManager/Monitoring/Monitoring.controller.js
+++ client/src/app/components/portal/MonitorManager/Monitoring/Monitoring.controller.js
@@ -2,7 +2,7 @@
 
 angular.module('BeehivePortal.MonitorManager')
 
-.controller('MonitoringController', ['$scope', '$rootScope', '$state', '$stateParams', '$location', '$$User', 'WebSocketClient', function($scope, $rootScope, $state, $stateParams, $location, $$User, WebSocketClient) {
+.controller('MonitoringController', ['$scope', '$rootScope', '$state', '$stateParams', '$location', '$$User', 'WebSocketClient', 'StateType', '$timeout', function($scope, $rootScope, $state, $stateParams, $location, $$User, WebSocketClient, StateType, $timeout) {
     if ($stateParams.id === 0) {
         $state.go('^');
     }
@@ -11,14 +11,14 @@ angular.module('BeehivePortal.MonitorManager')
     $scope.view = $stateParams;
 
     // get monitoring view detail
-    // $$User.getCustomData({ name: 'mv_' + $scope.view.id }).$promise.then(function(res) {
-    // $scope.view = res.view || {};
-    // websocketInit();
+    $$User.getCustomData({ name: 'mv_' + $scope.view.id }).$promise.then(function(res) {
+        $scope.view = res.view || {};
+        websocketInit();
+    })
 
-    $scope.view = { "id": 1470032672569, "name": "monitor 7", "desc": "7", "count": 0, "createDate": 1470032672569, "modifyDate": 1470032672569, "detail": [{ "modifyBy": "493e83c9", "vendorThingID": "0807W-F02-03-118", "kiiThingID": "th.f83120e36100-bada-6e11-81f4-0a4538a6", "select": false, "modifyDate": 1469116800000, "type": "Lighting", "tags": [], "kiiAppID": "493e83c9", "fullKiiThingID": "493e83c9-th.f83120e36100-bada-6e11-81f4-0a4538a6", "createBy": "f1dadad0-4834-11e6-9c73-00163e007aba", "globalThingID": 1090, "createDate": 1469030400000, "status": { "taiwanNo1": 1469168587000, "Power": 0, "target": "th.f83120e36100-bada-6e11-81f4-0a4538a6" } }, { "modifyBy": "493e83c9", "vendorThingID": "0807W-F02-15-118", "kiiThingID": "th.aba700e36100-72a9-6e11-81f4-0b7146b6", "select": false, "modifyDate": 1469116800000, "type": "Lighting", "tags": [], "kiiAppID": "493e83c9", "fullKiiThingID": "493e83c9-th.aba700e36100-72a9-6e11-81f4-0b7146b6", "createBy": "f1dadad0-4834-11e6-9c73-00163e007aba", "globalThingID": 1091, "createDate": 1469030400000, "status": { "taiwanNo1": 1469168580000, "Bri": 284, "target": "th.aba700e36100-72a9-6e11-81f4-0b7146b6" } }, { "modifyBy": "493e83c9", "vendorThingID": "0807W-F02-03-117", "kiiThingID": "th.aba700e36100-72a9-6e11-81f4-071aaac6", "select": false, "modifyDate": 1469116800000, "type": "Lighting", "tags": [], "kiiAppID": "493e83c9", "fullKiiThingID": "493e83c9-th.aba700e36100-72a9-6e11-81f4-071aaac6", "createBy": "f1dadad0-4834-11e6-9c73-00163e007aba", "globalThingID": 1093, "createDate": 1469030400000, "status": { "SetBri": 0, "Turntimes": 0, "taiwanNo1": 1469168581000, "State": 0, "Alarm": 0, "Bri": 0, "Runtimes": 0, "Power": 0, "target": "th.aba700e36100-72a9-6e11-81f4-071aaac6" } }, { "createDate": 1469721600000, "modifyDate": 1470067200000, "createBy": "640", "modifyBy": "2", "vendorThingID": "0807W-F02-15-220", "kiiAppID": "192b49ce", "type": "Sensor", "status": { "taiwanNo1": 1470121795000, "Bri": 256, "target": "th.aba700e36100-72a9-6e11-1c75-0870ee47" }, "fullKiiThingID": "192b49ce-th.aba700e36100-72a9-6e11-1c75-0870ee47", "schemaName": "Sensor", "schemaVersion": "1", "kiiThingID": "th.aba700e36100-72a9-6e11-1c75-0870ee47", "tags": [], "globalThingID": 1136 }] };
+    // $scope.view = { "id": 1470032672569, "name": "monitor 7", "desc": "7", "count": 0, "createDate": 1470032672569, "modifyDate": 1470032672569, "detail": [{ "modifyBy": "493e83c9", "vendorThingID": "0807W-F02-03-118", "kiiThingID": "th.f83120e36100-bada-6e11-81f4-0a4538a6", "select": false, "modifyDate": 1469116800000, "type": "Lighting", "tags": [], "kiiAppID": "493e83c9", "fullKiiThingID": "493e83c9-th.f83120e36100-bada-6e11-81f4-0a4538a6", "createBy": "f1dadad0-4834-11e6-9c73-00163e007aba", "globalThingID": 1090, "createDate": 1469030400000, "status": { "taiwanNo1": 1469168587000, "Power": 0, "target": "th.f83120e36100-bada-6e11-81f4-0a4538a6" } }, { "modifyBy": "493e83c9", "vendorThingID": "0807W-F02-15-118", "kiiThingID": "th.aba700e36100-72a9-6e11-81f4-0b7146b6", "select": false, "modifyDate": 1469116800000, "type": "Lighting", "tags": [], "kiiAppID": "493e83c9", "fullKiiThingID": "493e83c9-th.aba700e36100-72a9-6e11-81f4-0b7146b6", "createBy": "f1dadad0-4834-11e6-9c73-00163e007aba", "globalThingID": 1091, "createDate": 1469030400000, "status": { "taiwanNo1": 1469168580000, "Bri": 284, "target": "th.aba700e36100-72a9-6e11-81f4-0b7146b6" } }, { "modifyBy": "493e83c9", "vendorThingID": "0807W-F02-03-117", "kiiThingID": "th.aba700e36100-72a9-6e11-81f4-071aaac6", "select": false, "modifyDate": 1469116800000, "type": "Lighting", "tags": [], "kiiAppID": "493e83c9", "fullKiiThingID": "493e83c9-th.aba700e36100-72a9-6e11-81f4-071aaac6", "createBy": "f1dadad0-4834-11e6-9c73-00163e007aba", "globalThingID": 1093, "createDate": 1469030400000, "status": { "SetBri": 0, "Turntimes": 0, "taiwanNo1": 1469168581000, "State": 0, "Alarm": 0, "Bri": 0, "Runtimes": 0, "Power": 0, "target": "th.aba700e36100-72a9-6e11-81f4-071aaac6" } }, { "createDate": 1469721600000, "modifyDate": 1470067200000, "createBy": "640", "modifyBy": "2", "vendorThingID": "0807W-F02-15-220", "kiiAppID": "192b49ce", "type": "Sensor", "status": { "taiwanNo1": 1470121795000, "Bri": 256, "target": "th.aba700e36100-72a9-6e11-1c75-0870ee47" }, "fullKiiThingID": "192b49ce-th.aba700e36100-72a9-6e11-1c75-0870ee47", "schemaName": "Sensor", "schemaVersion": "1", "kiiThingID": "th.aba700e36100-72a9-6e11-1c75-0870ee47", "tags": [], "globalThingID": 1136 }] };
 
-    setTimeout(function() { websocketInit(); }, 1000);
-    // })
+    // setTimeout(function() { websocketInit(); }, 3000);
 
     // websocket connection
     function websocketInit() {
@@ -27,15 +27,16 @@ angular.module('BeehivePortal.MonitorManager')
             subscription($scope.view.detail[i]);
         }
 
-        WebSocketClient.subscribe('/topic/192b49ce/th.aba700e36100-72a9-6e11-4785-0b1ad90b', function(res) {
+        // WebSocketClient.subscribe('/topic/192b49ce/th.aba700e36100-72a9-6e11-4785-0b1ad90b', function(res) {
 
-        });
+        // });
     }
 
     // thing subscription
     function subscription(thing) {
         WebSocketClient.subscribe('/topic/' + thing.kiiAppID + '/' + thing.kiiThingID, function(res) {
             thing.state = parseState(JSON.parse(res.body));
+            $timeout(function() { waterfall('.card-columns') }, 0);
         });
     }
 
@@ -43,6 +44,7 @@ angular.module('BeehivePortal.MonitorManager')
     function parseState(state) {
         var ret = [];
         for (var k in state.state) {
+            if (!StateType[k]) continue;
             ret.push({
                 'name': k,
                 'value': state.state[k]
