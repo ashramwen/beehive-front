e27bc55a51c6d75e2f8765522d694115fbb36d48
augustine <ashramwen@gmail.com>
2016-08-04 14:46:43 +0800

    refine monitoring

--- client/src/app/components/portal/MonitorManager/MonitorManager.filter.js
+++ client/src/app/components/portal/MonitorManager/MonitorManager.filter.js
@@ -2,13 +2,6 @@
 
 angular.module('BeehivePortal.MonitorManager')
 
-.filter('locTag', function() {
-    return function(loc) {
-        var temp = loc.split('-');
-        return temp[0] + '-' + temp[1];
-    };
-})
-
 .filter('typeName', ['ThingType', function(ThingType) {
     return function(type) {
         return ThingType[type] || type;
