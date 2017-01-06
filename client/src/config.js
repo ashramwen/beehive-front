a2747c939b9bf239e73f5d96faebcbb32e83b631
ytyszxf <ljz135790@gmail.com>
2017-01-06 11:26:14 +0800

    config

--- client/src/config.js
+++ client/src/config.js
@@ -20,6 +20,6 @@
             "siteUrl": "http://localhost:9090/beehive-portal",
             "thirdPartyAPIUrl": "http://114.215.178.24:8081/3rdpartyapiserver/app/api_inquiry/apilist.html?vendor=Beehive"
         },
-        "ENV": "QA"
+        "ENV": "DEV"
     };
 })();
