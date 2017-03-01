b46f95df4909ae090f89c22a9224634ace090cc4
ytyszxf <ljz135790@gmail.com>
2017-03-01 10:02:11 +0800

    save

--- client/src/app/shared/config.js
+++ client/src/app/shared/config.js
@@ -9,6 +9,7 @@
     window.AppConfig = appConfig[appConfig.ENV];
     window.thirdPartyAPIUrl = appConfig[appConfig.ENV].thirdPartyAPIUrl;
     var cloudUrl = appConfig[appConfig.ENV].cloudUrl;
+    console.log(appConfig.version);
     /*
      * API used globally for web service
      * by George Lin
