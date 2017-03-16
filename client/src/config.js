236bf133f5212a22a80acb909b1d5e148993373d
ytyszxf <ljz135790@gmail.com>
2017-03-16 14:55:15 +0800

    config

--- client/src/config.js
+++ client/src/config.js
@@ -21,11 +21,12 @@
             "thirdPartyAPIUrl": "http://114.215.178.24:8081/3rdpartyapiserver/app/api_inquiry/apilist.html?vendor=Beehive"
         },
         "PRODUCTION": {
-            "cloudUrl": "https://api-beehivecn4.kii.com",
-            "wsUrl": "ws://120.77.83.143:8080/beehive-portal/websocket/stomp",
-            "siteUrl": "http://120.77.83.143:8080/beehive-portal",
-            "thirdPartyAPIUrl": "http://120.77.83.143:9082/cn/guide"
-        },
+           "cloudUrl": "https://api-beehivecn4.kii.com",
+           "wsUrl": "ws://120.77.83.143:8080/beehive-portal/websocket/stomp",
+           "siteUrl": "http://120.77.83.143:8080/beehive-portal",
+           "kiiAppID": "460aebd7",
+           "thirdPartyAPIUrl": "http://120.77.83.143:9082/cn/guide"
+       },
         "ENV": "DEV",
         "version": '1.0'
     };
